9576c951298b0db54b6310b72b22eee7b021cd58
Sardicus <enes.bilginf@gmail.com>
2026-02-22 11:44:04 +0300

    feat: implement cors config

--- src/main/java/com/naira/evalroi/controller/AuthController.java
+++ src/main/java/com/naira/evalroi/controller/AuthController.java
@@ -3,28 +3,16 @@ package com.naira.evalroi.controller;
 import com.naira.evalroi.dto.auth.AuthResponseDto;
 import com.naira.evalroi.dto.auth.LoginDto;
 import com.naira.evalroi.dto.auth.RegisterDto;
-import com.naira.evalroi.entity.Role;
-import com.naira.evalroi.entity.UserEntity;
-import com.naira.evalroi.enums.LoginWith;
-import com.naira.evalroi.enums.RoleEnum;
-import com.naira.evalroi.repository.RoleRepository;
-import com.naira.evalroi.repository.UserRepository;
 import com.naira.evalroi.security.JWTGenerator;
 import com.naira.evalroi.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
 
 @RestController
 @RequestMapping("api/v1/auth")
